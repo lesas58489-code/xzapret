b35c116c80ad40b61e001895b8c387287c1d17f7
Vitaly Sokolov <vitalysokolov9751@gmail.com>
2026-04-12 14:53:08 +0000

    Add tun2socks.aar (Go engine, arm64) + gradle dependency

--- clients/android/app/build.gradle.kts
+++ clients/android/app/build.gradle.kts
@@ -36,4 +36,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation(files("libs/tun2socks.aar"))
 }
