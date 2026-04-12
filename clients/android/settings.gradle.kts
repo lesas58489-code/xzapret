a4f02c8bbc3237f8f17cdcd2ee99599190eb921c
Vitaly Sokolov <vitalysokolov9751@gmail.com>
2026-04-12 10:17:47 +0000

    Fix Gradle: remove dependencyResolutionManagement, use allprojects

--- clients/android/settings.gradle.kts
+++ clients/android/settings.gradle.kts
@@ -6,14 +6,5 @@ pluginManagement {
     }
 }
 
-@Suppress("UnstableApiUsage")
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "XZAPClient"
 include(":app")
