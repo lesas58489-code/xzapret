b5da0247bfe0c49d20bc95a721e131e0df810c2c
Vitaly Sokolov <vitalysokolov9751@gmail.com>
2026-04-29 09:04:49 +0000

    fix: import InfiniteTransition.animateFloat extension

--- clients/android/app/src/main/java/com/xzap/client/ui/XZapretButton.kt
+++ clients/android/app/src/main/java/com/xzap/client/ui/XZapretButton.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
